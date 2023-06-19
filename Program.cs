54b780ada47be5e3fe14e12568f0d7df45ef760f
Oleg <teleginos@mail.ru>
2023-06-19 21:58:05 +0300

    added a method for reading the original array of strings from the keyboard

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] array = ReadArrayFromInput();
+       
+    }
+
+    static string[] ReadArrayFromInput()
+    {
+        Console.WriteLine("Введите количество элементов массива: ");
+        int size = int.Parse(Console.ReadLine());
+
+        string[] array = new string[size];
+        Console.WriteLine("Введите элементы массива (каждый с новой строки): ");
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+
+        return array;
+    }
+}
+
